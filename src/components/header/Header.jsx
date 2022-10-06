518a30856a2dffdbcd814086f94b300e2d5fd78b
Anan Diljar <anandiljar5@gmail.com>
2022-10-06 11:57:18 +0600

    Added the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hi, it's me</h5>
+        <h1>Mario</h1>
+        <h5 className="text-light">Sagol Pagol</h5>
+        <CTA />
+        <HeaderSocial /> 
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
